f19970e37bf7ad2f4271c312fba8fe857b7c566a
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-01-04 15:33:08 +0000

    Initial commit of Kaggle:Titanic competition
    This commit contains the training and test data sets, along with an
    example submission file formatting example, as well as an initial code
    file with comments to describe the general initially intended structure
    of the final code.

---
+++ Kaggle/Titanic/code.py
@@ -0,0 +1,20 @@
+# Imports
+
+
+# Data loading 
+
+
+# Data preprocessing and putting into a data structure that can be 
+# used by a classifier
+
+
+# Create classifier with parameters
+
+
+# Train classifier
+
+
+# Load test dataset and use classifer to make predictions
+
+
+# Save test predictions to disk in the format required for kaggle. 
