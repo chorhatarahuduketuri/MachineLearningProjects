cc7570d611cc1a84c5c170d5c0c140ac86dfd35b
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-01-09 17:35:04 +0000

    Data cleanup code added
    The age, sex, and embarked columns have been cleaned up and converted
    into numeric forms so they can be used.

--- Kaggle/Titanic/code.py
+++ Kaggle/Titanic/code.py
@@ -1,11 +1,18 @@
 # Imports
-
+import pandas as pd
 
 # Data loading 
-
+training_data = pd.read_csv('train.csv')
 
 # Data preprocessing and putting into a data structure that can be 
 # used by a classifier
+training_data["Age"] = training_data["Age"].fillna(training_data["Age"].median())
+training_data["Sex"].loc[training_data["Sex"] == "male"] = 0
+training_data["Sex"].loc[training_data["Sex"] == "female"] = 1
+training_data["Embarked"].fillna(0)
+training_data["Embarked"].loc[training_data["Embarked"] == "C"] = 1
+training_data["Embarked"].loc[training_data["Embarked"] == "Q"] = 2
+training_data["Embarked"].loc[training_data["Embarked"] == "S"] = 3
 
 
 # Create classifier with parameters
