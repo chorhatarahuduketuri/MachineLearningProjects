7ded6fded315443ae0961242cf19a664c6ce037e
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-01-11 15:57:12 +0000

    First submission - Simple Logistic Regression
    This commit contains the addition of test data cleaning, as well as the
    training of a logistic regression model, and the code to create a valid
    submission file. It also contains the first submission itself.

--- Kaggle/Titanic/code.py
+++ Kaggle/Titanic/code.py
@@ -1,5 +1,6 @@
 # Imports
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 # Data loading 
 training_data = pd.read_csv('train.csv')
@@ -9,19 +10,37 @@ training_data = pd.read_csv('train.csv')
 training_data["Age"] = training_data["Age"].fillna(training_data["Age"].median())
 training_data["Sex"].loc[training_data["Sex"] == "male"] = 0
 training_data["Sex"].loc[training_data["Sex"] == "female"] = 1
-training_data["Embarked"].fillna(0)
+training_data["Embarked"] = training_data["Embarked"].fillna(0)
 training_data["Embarked"].loc[training_data["Embarked"] == "C"] = 1
 training_data["Embarked"].loc[training_data["Embarked"] == "Q"] = 2
 training_data["Embarked"].loc[training_data["Embarked"] == "S"] = 3
 
+X = training_data[["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]]
+y = training_data["Survived"]
 
 # Create classifier with parameters
-
+logRegClasif = LogisticRegression(max_iter=500, solver='lbfgs')
 
 # Train classifier
-
+logRegClasif.fit(X,y)
 
 # Load test dataset and use classifer to make predictions
+test_data = pd.read_csv('test.csv')
 
+test_data["Age"] = test_data["Age"].fillna(test_data["Age"].median())
+test_data["Sex"].loc[test_data["Sex"] == "male"] = 0
+test_data["Sex"].loc[test_data["Sex"] == "female"] = 1
+test_data["Embarked"] = test_data["Embarked"].fillna(0)
+test_data["Embarked"].loc[test_data["Embarked"] == "C"] = 1
+test_data["Embarked"].loc[test_data["Embarked"] == "Q"] = 2
+test_data["Embarked"].loc[test_data["Embarked"] == "S"] = 3
+test_data["Fare"] = test_data["Fare"].fillna(0)
 
-# Save test predictions to disk in the format required for kaggle. 
+X_test = test_data[["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]]
+
+predictions = logRegClasif.predict(X_test)
+
+# Save test predictions to disk in the format required for kaggle.
+submission = pd.DataFrame({"PassengerId": test_data["PassengerId"], "Survived": predictions})
+submission.to_csv("submission.csv", index=False)
+ 
