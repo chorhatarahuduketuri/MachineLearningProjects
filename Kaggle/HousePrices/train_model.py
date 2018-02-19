eca94c79789c9cf4968f235cc25a1b22b355b13d
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-02-19 20:38:34 +0000

    HousePrices: feature engineering in train_model.py
    In train_model.py, training data has been loaded, features with too
    many null values have been dropped, and work to remove a single data
    sample with a null value for the 'Electrical' feature is underway.
    
    Code for the numericalisation of categorical features, mean
    normalisation, and linear regression model creation are also present.

--- Kaggle/HousePrices/train_model.py
+++ Kaggle/HousePrices/train_model.py
@@ -1,15 +1,34 @@
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
-traincsv = pd.read_csv('train.csv')
-testcsv = pd.read_csv('test.csv')
 
-# TODO: get the categorical data into a numerical form (use the dummy function)
-# TODO: mean normalize the numerical data
-# TODO: prepare logistic regression model
-# TODO: train logistic regression model
-# TODO: test logistic regression model
+# load the training data
+traincsv = pd.read_csv('train.csv')
+
+# remove all features that are missing from >1% of data samples (as per eda.py output)
+traincsv.drop(['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu', 'LotFrontage', 'GarageCond', 'GarageType',
+               'GarageYrBlt', 'GarageFinish', 'GarageQual', 'BsmtExposure', 'BsmtFinType2', 'BsmtFinType1', 'BsmtCond',
+               'BsmtQual', 'MasVnrArea', 'MasVnrType'], axis=1)
+# TODO: delete all data samples that have missing feature data (where the feature data is missing from <1% of all data samples)
+indexOfNullElectricalSample = traincsv.loc[traincsv['Electrical'].isnull()].index
+traincsv.drop(index=indexOfNullElectricalSample)
+
+# get the categorical data into a numerical form (use the dummy function)
+traincsv = pd.get_dummies(traincsv)
+
+# mean normalize the numerical data
+scaler = StandardScaler()
+scaler.fit(traincsv)
+mn_train = scaler.transform(traincsv)
+
+# prepare linear regression model
+linearRegression = LinearRegression(copy_X=True)
+
+# TODO: train linear regression model
+# TODO: test linear regression model
 # TODO: prepare neural network model
 # TODO: train neural network model
 # TODO: test neural network model
+
+traincsv[['GarageCond', 'GarageType', 'GarageYrBlt', 'GarageFinish', 'GarageQual', 'BsmtExposure', 'BsmtFinType2',
+          'BsmtFinType1', 'BsmtCond', 'BsmtQual', 'MasVnrArea', 'MasVnrType', 'Electrical']]
