a8f9ff227b7bdde58706400679bf455f3d242a50
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-02-19 19:54:08 +0000

    HousePrices: add plan for train_model.py
    Added a plan for train_model.py in train_model.py in TODO comments in
    train_model.py.

--- Kaggle/HousePrices/train_model.py
+++ Kaggle/HousePrices/train_model.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-
+from sklearn.preprocessing import StandardScaler
 traincsv = pd.read_csv('train.csv')
 testcsv = pd.read_csv('test.csv')
 
+# TODO: get the categorical data into a numerical form (use the dummy function)
+# TODO: mean normalize the numerical data
+# TODO: prepare logistic regression model
+# TODO: train logistic regression model
+# TODO: test logistic regression model
+# TODO: prepare neural network model
+# TODO: train neural network model
+# TODO: test neural network model
