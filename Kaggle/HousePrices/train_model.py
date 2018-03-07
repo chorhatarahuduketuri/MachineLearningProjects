91ee1fde89ed508b973c430bf5afdd10c688dcc8
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-03-07 12:02:59 -0500

    HousePrices: finished data prep for linReg
    Finished data preparation for linear regression in train_model.py.
    
    All features with <1% data are removed, and all samples with NANs are
    removed, or the NANs have been filled in with the mean of that feature.
    
    All categorical features have been converted to binary fields, and
    StandardScaler has been used for mean normalisation.
    
    The data has been split into train/test groups using
    model_selection.train_test_split.

--- Kaggle/HousePrices/train_model.py
+++ Kaggle/HousePrices/train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 # load the training data
 traincsv = pd.read_csv('train.csv')
@@ -9,12 +10,15 @@ traincsv = pd.read_csv('train.csv')
 traincsv.drop(['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu', 'LotFrontage', 'GarageCond', 'GarageType',
                'GarageYrBlt', 'GarageFinish', 'GarageQual', 'BsmtExposure', 'BsmtFinType2', 'BsmtFinType1', 'BsmtCond',
                'BsmtQual', 'MasVnrArea', 'MasVnrType'], axis=1)
-# TODO: delete all data samples that have missing feature data (where the feature data is missing from <1% of all data samples)
+# delete all data samples that have missing feature data (where the feature data is missing from <1% of all data samples)
 indexOfNullElectricalSample = traincsv.loc[traincsv['Electrical'].isnull()].index
-traincsv.drop(index=indexOfNullElectricalSample)
+traincsv = traincsv.drop(index=indexOfNullElectricalSample)
 
 # get the categorical data into a numerical form (use the dummy function)
 traincsv = pd.get_dummies(traincsv)
+
+# fill in all remaining NAN values with the mean of that column
+traincsv = traincsv.fillna(traincsv.mean())
 
 # mean normalize the numerical data
 scaler = StandardScaler()
@@ -24,11 +28,13 @@ mn_train = scaler.transform(traincsv)
 # prepare linear regression model
 linearRegression = LinearRegression(copy_X=True)
 
+# create X and y for the training
+X = traincsv.drop('SalePrice', 1)
+y = traincsv['SalePrice']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(1 / 3.))
+
 # TODO: train linear regression model
 # TODO: test linear regression model
 # TODO: prepare neural network model
 # TODO: train neural network model
 # TODO: test neural network model
-
-traincsv[['GarageCond', 'GarageType', 'GarageYrBlt', 'GarageFinish', 'GarageQual', 'BsmtExposure', 'BsmtFinType2',
-          'BsmtFinType1', 'BsmtCond', 'BsmtQual', 'MasVnrArea', 'MasVnrType', 'Electrical']]
