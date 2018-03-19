005cf980b222ee498b7e853ca4d07a23fc8dceba
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-03-19 16:22:25 -0500

    HousePrices: add MLPClassifier and SVM.SVC
    This adds the the neural network classifier, in the form of an
    MLPClassifier (it has very poor performance), and an SVM classifier,
    in the form of a SVC classifier (which also has very poor performance).
    
    Additionally, due to the variable performance of linear regression
    depending on the selection of training and test sets, the process is
    put into a loop which exits on the linear regression classifier getting
    a score of 0.88 or above.

--- Kaggle/HousePrices/train_model.py
+++ Kaggle/HousePrices/train_model.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm
 
 # load the training data
 traincsv = pd.read_csv('train.csv')
@@ -20,26 +21,38 @@ traincsv = pd.get_dummies(traincsv)
 # fill in all remaining NAN values with the mean of that column
 traincsv = traincsv.fillna(traincsv.mean())
 
-# mean normalize the numerical data
-scaler = StandardScaler()
-scaler.fit(traincsv)
-mn_train = scaler.transform(traincsv)
-
-# prepare linear regression model
-linearRegression = LinearRegression(copy_X=True)
-
 # create X and y for the training
 X = traincsv.drop('SalePrice', 1)
 y = traincsv['SalePrice']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(1 / 3.))
 
-# train linear regression model
-linearRegression.fit(X_train, y_train)
+# Linear Regression
+linearRegression = LinearRegression(copy_X=True)
+scoreNotHighEnough = True
+while scoreNotHighEnough:
+    # Create separate test/train data sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(1 / 3.))
 
-# test linear regression model
-linearRegressionScore = linearRegression.score(X_test, y_test)
-print('Linear Regression Score: ' + str(linearRegressionScore))
+    # Create and train the linear regression model
+    linearRegression.fit(X_train, y_train)
 
-# TODO: prepare neural network model
-# TODO: train neural network model
-# TODO: test neural network model
+    # test linear regression model
+    linearRegressionScore = linearRegression.score(X_test, y_test)
+    print('Linear Regression Score: ' + str(linearRegressionScore))
+    if linearRegressionScore > 0.88:
+        scoreNotHighEnough = False
+
+# prepare neural network model
+mlpClassifier = MLPClassifier(hidden_layer_sizes=(289, 289), activation='logistic', solver='sgd')
+# train neural network model
+mlpClassifier.fit(X_train, y_train)
+# test neural network model
+mlpClassifierScore = mlpClassifier.score(X_test, y_test)
+print('MLPClassifier score: ' + str(mlpClassifierScore))
+
+# prepare SVM model
+svmClassifier = svm.SVC(kernel='poly')
+# train SVM model
+svmClassifier.fit(X_train, y_train)
+# test SVM model
+svmClassifierScore = svmClassifier.score(X_test, y_test)
+print('SVM SVC score: ' + str(svmClassifierScore))
