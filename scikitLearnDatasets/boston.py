7451aa1eedc1c1da3a8ee9510ff02835d42d9ee3
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-05-11 13:09:02 -0700

    Boston: Code to step 5, write up to step 5

--- scikitLearnDatasets/boston.py
+++ scikitLearnDatasets/boston.py
@@ -8,7 +8,6 @@ boston = datasets.load_boston()
 data = boston.data
 target = boston.target
 
-
 # Step 3
 print('data.shape:')
 print(data.shape)
@@ -43,3 +42,57 @@ ax2.set(title='Distribution of the ' + feature_name_NOX + ' data',
         xlabel=feature_name_NOX + ' measurements')
 fig2.savefig('gitIgnoreDir/boston/' + feature_name_NOX + '.png')
 
+# Step 4 - Model selection and feature engineering
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler().fit(X_train)
+standardised_X = scaler.transform(X_train)
+standardised_X_test = scaler.transform(X_test)
+
+from sklearn.preprocessing import PolynomialFeatures
+
+# 2nd degree polynomials
+poly_2 = PolynomialFeatures(2)
+poly_2.fit(standardised_X)
+X_train_poly_2 = poly_2.transform(standardised_X)
+X_test_poly_2 = poly_2.transform(standardised_X_test)
+
+# Step 5 - Model creation and training
+# Linear Regression
+from sklearn.linear_model import LinearRegression
+
+lr1 = LinearRegression()
+lr2 = LinearRegression()
+
+lr1.fit(X_train, y_train)
+lr2.fit(X_train_poly_2, y_train)
+
+lr1_pred = lr1.predict(X_test)
+lr2_pred = lr2.predict(X_test_poly_2)
+
+# Support Vector Machine
+from sklearn.svm import SVR
+
+svr1 = SVR()
+svr2 = SVR()
+
+svr1.fit(X_train, y_train)
+svr2.fit(X_train_poly_2, y_train)
+
+svr1_pred = svr1.predict(X_test)
+svr2_pred = svr2.predict(X_test_poly_2)
+
+# Step 6 - model evaluation
+from sklearn.metrics import mean_absolute_error
+
+mean_absolute_error(y_test, lr1_pred)
+
+mean_absolute_error(y_test, lr2_pred)
+
+mean_absolute_error(y_test, svr1_pred)
+
+mean_absolute_error(y_test, svr2_pred)
