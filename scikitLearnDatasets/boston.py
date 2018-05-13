d303b20fe43f6bdfa81b1df8011bef7e22f7e1e4
chorhatarahuduketuri <chorhatarahuduketuri@gmail.com>
2018-05-13 16:07:51 -0700

    Boston: COMPLETE
    The Analysis is complete. This was the first project so far where step
    7 resulted in revisiting steps 5 and 6. Also, it was the first project
    where ANNs were the clear winner.

--- scikitLearnDatasets/boston.py
+++ scikitLearnDatasets/boston.py
@@ -68,10 +68,10 @@ from sklearn.linear_model import LinearRegression
 lr1 = LinearRegression()
 lr2 = LinearRegression()
 
-lr1.fit(X_train, y_train)
+lr1.fit(standardised_X, y_train)
 lr2.fit(X_train_poly_2, y_train)
 
-lr1_pred = lr1.predict(X_test)
+lr1_pred = lr1.predict(standardised_X_test)
 lr2_pred = lr2.predict(X_test_poly_2)
 
 # Support Vector Machine
@@ -80,19 +80,47 @@ from sklearn.svm import SVR
 svr1 = SVR()
 svr2 = SVR()
 
-svr1.fit(X_train, y_train)
+svr1.fit(standardised_X, y_train)
 svr2.fit(X_train_poly_2, y_train)
 
-svr1_pred = svr1.predict(X_test)
+svr1_pred = svr1.predict(standardised_X_test)
 svr2_pred = svr2.predict(X_test_poly_2)
 
 # Step 6 - model evaluation
 from sklearn.metrics import mean_absolute_error
 
-mean_absolute_error(y_test, lr1_pred)
+print('Mean absolute error, logistic regression, 1st order training data, in steps of $1000: ')
+print(mean_absolute_error(y_test, lr1_pred))
+print('Mean absolute error, logistic regression, 2nd order training data, in steps of $1000: ')
+print(mean_absolute_error(y_test, lr2_pred))
+print('Mean absolute error, SVM, 1st order training data, in steps of $1000: ')
+print(mean_absolute_error(y_test, svr1_pred))
+print('Mean absolute error, SVM, 2nd order training data, in steps of $1000: ')
+print(mean_absolute_error(y_test, svr2_pred))
 
-mean_absolute_error(y_test, lr2_pred)
+# Step 5 - take 2
+from sklearn.neural_network import MLPRegressor
 
-mean_absolute_error(y_test, svr1_pred)
+ann1 = MLPRegressor(max_iter=5000, hidden_layer_sizes=(13, 13))
+ann2 = MLPRegressor(max_iter=5000, hidden_layer_sizes=(105, 105))
 
-mean_absolute_error(y_test, svr2_pred)
+ann1.fit(standardised_X, y_train)
+ann2.fit(X_train_poly_2, y_train)
+
+ann_1_pred = ann1.predict(standardised_X_test)
+ann_2_pred = ann2.predict(X_test_poly_2)
+
+print('Mean absolute error, ANN, 1st order training data, in steps of $1000: ')
+print(mean_absolute_error(y_test, ann_1_pred))
+print('Mean absolute error, ANN, 2nd order training data, in steps of $1000: ')
+print(mean_absolute_error(y_test, ann_2_pred))
+
+# Save the models somewhere
+from sklearn.externals import joblib
+
+joblib.dump(lr1, 'gitIgnoreDir/boston/lr_default_1.pkl')
+joblib.dump(lr2, 'gitIgnoreDir/boston/lr_default_2.pkl')
+joblib.dump(svr1, 'gitIgnoreDir/boston/svr_default_1.pkl')
+joblib.dump(svr2, 'gitIgnoreDir/boston/svr_default_2.pkl')
+joblib.dump(ann1, 'gitIgnoreDir/boston/ann_1.pkl')
+joblib.dump(ann2, 'gitIgnoreDir/boston/ann_2.pkl')
